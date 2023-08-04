2efdb5d3c52ae9660e0e07134ed34b55c24ca63f
rlaalswns1 <124165380+rlaalswns1@users.noreply.github.com>
2023-08-04 17:45:54 +0900

    (2023.08.04)애니메이션 클립

---
+++ UnityTutorial/Assets/Unity Animation Clip/Script/lteration.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lteration : MonoBehaviour
+{
+    #region 반복문
+    //프로그램 내에서 특정한 작업을 반복적으로 수행하는 명령문
+
+    #endregion
+
+    void Start()
+    {
+        //for문
+        //초기식을 연산하여 조건식의 결과에 따라
+        //특정한 횟수만큼 반복하는 반복문
+        for (int i = 0; i < 5; i++)
+        {
+            Debug.Log("1");
+        }
+    }
+
+}
